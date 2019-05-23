f78a629fd301cf56da4459d9d57f03d087198441
Johannes <jwoeber@riegl.com>
2019-05-23 12:13:34 +0000

    tested 32 requests

--- example/userspace_dma_driver_test.cpp
+++ example/userspace_dma_driver_test.cpp
@@ -31,18 +31,25 @@ int main()
         {
                 reinterpret_cast<uint8_t *>(driver->getBuffer())[i] = i % 256;
         }
+        printf("Setting values of the last %d Bytes of the Buffer to 0 \n", driver->getBufferSize()/2);
+        for (size_t i = 0; i < driver->getBufferSize()/2; ++i)
+        {
+                reinterpret_cast<uint8_t *>(driver->getBuffer())[i+driver->getBufferSize()/2] = i % 256;
+        }
 
-        DmaDriver::transferRequest request1 = { 
-                .srcAddr = driver->getBufferPhysAddr(),
-                .destAddr = driver->getBufferPhysAddr() + driver->getBufferSize()/2,
-                .transferLength = driver->getBufferSize()/4
-        };
-        DmaDriver::transferRequest request2 = { 
-                .srcAddr = driver->getBufferPhysAddr() + driver->getBufferSize()/4,
-                .destAddr = driver->getBufferPhysAddr() + driver->getBufferSize()/2 + driver->getBufferSize()/4,
-                .transferLength = driver->getBufferSize()/4
-        };
-        driver->configureDMA( {request1, request2} );
+
+        vector<DmaDriver::transferRequest> requests;
+        for( int i = 0; i < 32; ++i ){
+                const unsigned int requestSize = (driver->getBufferSize() / 2) / 32;
+                DmaDriver::transferRequest request = { 
+                        .srcAddr = driver->getBufferPhysAddr() + i*requestSize,
+                        .destAddr = driver->getBufferPhysAddr() + driver->getBufferSize()/2 + i*requestSize,
+                        .transferLength = requestSize
+                };  
+                requests.push_back(request);
+        }
+       
+        driver->configureDMA( requests );
         auto start_time = std::chrono::high_resolution_clock::now();
 
 
